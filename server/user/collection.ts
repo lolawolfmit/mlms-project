efe6aaf29bb0bed59b75a18b587a9c161d672d17
Malobika Syed <42706046+mfsyed@users.noreply.github.com>
2022-11-22 22:08:51 -0500

    debugged delete functionality
    forgot to do save

--- server/user/collection.ts
+++ server/user/collection.ts
@@ -91,6 +91,7 @@ class UserCollection {
   static async deleteOne(userId: Types.ObjectId | string): Promise<boolean> {
     const user = await UserModel.findOne({_id: userId});
     user.deletedStatus = true;
+    await user.save();
     return user.deletedStatus;
   }
 
@@ -163,7 +164,7 @@ class UserCollection {
     const followee = await UserModel.findOne({_id: followeeId});
 
     follower.following.push(followee);
-
+    await follower.save();
   }
 
 
